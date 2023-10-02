52cc0898bebec8964e7be595fa5f4d6db40ad544
GaliciaAlex <146693264+GaliciaAlex@users.noreply.github.com>
2023-10-01 22:13:43 -0600

    primer punto de venta

---
+++ Eagles_pos/Config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+const base_url="http://localhost/Eagles_pos/";
+const host= "localhost";
+const user="id21339316_alexvic";
+const pass="Zimmer2000$";
+const db= "id21339316_alejandro";
+const charset= "charset=utf8";
+?>
